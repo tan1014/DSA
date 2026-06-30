466d1bfc1a66b8b6c52c16014a31fae2800d32d1
Tanu <151056036+tan1014@users.noreply.github.com>
2026-06-30 10:32:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (87.13%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int n) {
+        int a = (int)Math.sqrt(n);
+        if(a * a == n ){
+            return true;
+        }else{
+            return false;
+        }
+        
+
+
+
+
+        
+    }
+}
